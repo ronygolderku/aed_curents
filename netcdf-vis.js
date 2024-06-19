29822fbf701e90a8bf2b5705d60e65a11c0b7b5a
ronygolderku <ronygolderku@gmail.com>
2024-06-20 00:21:27 +0800

    slider

--- netcdf-vis.js
+++ netcdf-vis.js
@@ -1,47 +1,16 @@
-// Load Leaflet map
-
-function initDemoMap(){
-
-    // Old baselayers
-    /*
-    var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, ' +
-        'NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-    });
-    var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    	maxZoom: 19,
-    	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    var Esri_DarkGreyCanvas = L.tileLayer(
-        "https://{s}.sm.mapstack.stamen.com/" +
-        "(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/" +
-        "{z}/{x}/{y}.png",
-        {
-            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, ' +
-            'NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-        }
-    );
-
-    */
-
-    // New baselayers
-    var Esri_DarkGreyCanvas = L.esri.basemapLayer('DarkGray')
-    var Esri_WorldImagery = L.esri.basemapLayer('Imagery')
+function initDemoMap() {
+    var Esri_DarkGreyCanvas = L.esri.basemapLayer('DarkGray');
+    var Esri_WorldImagery = L.esri.basemapLayer('Imagery');
     var baseLayers = {
         "Grey Canvas": Esri_DarkGreyCanvas,
         "Satellite": Esri_WorldImagery,
     };
-
-
-    // set map bounds
-    // var corner1 = L.latLng(-89.98155760646617, -180.0),
-    // corner2 = L.latLng(89.99346179538875, 180.0),
     var corner1 = L.latLng(-30, 110),
-    corner2 = L.latLng(-35, 120),
-    bounds = L.latLngBounds(corner1, corner2);
+        corner2 = L.latLng(-35, 120),
+        bounds = L.latLngBounds(corner1, corner2);
 
     var map = L.map('map', {
-        layers: [ Esri_DarkGreyCanvas ],
+        layers: [Esri_DarkGreyCanvas],
         center: bounds.getCenter(),
         zoom: 9,
         minZoom: 3,
@@ -50,48 +19,123 @@ function initDemoMap(){
         maxBoundsViscosity: 1
     });
 
-    // layer control panal
-    var layerControl = L.control.layers(baseLayers,null,{collapsed:false});
+    var layerControl = L.control.layers(baseLayers, null, { collapsed: false });
     layerControl.addTo(map);
-    // map.setView([0, 0], 1);
 
-    // idw-map
-    // var idw = L.idwLayer(addressPoints,{
-    //         opacity: 0.3,
-    //         maxZoom: 18,
-    //         cellSize: 5,
-    //         exp: 3,
-    //         max: 75
-    //     })
-    //     //idw.addTo(map);
-    // layerControl.addOverlay(idw, 'Global Temp').addTo(map)
-    // L.control.layers(overlays,null,{collapsed:false}).addTo(map);
+    // Ensure the slider remains on top
+    map.on('zoomend', function() {
+        document.getElementById('time-slider-container').style.zIndex = 1000;
+    });
 
     return {
         map: map,
         layerControl: layerControl
     };
-
 }
 
-// leaflet-velocity layer
 var mapStuff = initDemoMap();
 var map = mapStuff.map;
 var layerControl = mapStuff.layerControl;
 
-// load data (u, v grids) from somewhere (e.g. https://github.com/danwild/wind-js-server)
-$.getJSON('data/wind.json', function (data) {
+var times = [];
+var dataCache = {};
+var velocityLayer = null;
 
-	var velocityLayer = L.velocityLayer({
-		displayValues: true,
-		displayOptions: {
-			velocityType: 'Wind',
-			displayPosition: 'bottomleft',
-			displayEmptyString: 'No wind data'
-		},
-		data: data,
-		maxVelocity: 10
-	});
+// Load all JSON files
+for (let i = 1; i <= 13; i++) {
+    $.getJSON(`data/wind${i}.json`, function (data) {
+        const refTime = data[0].header.refTime;
+        times.push(refTime);
+        dataCache[refTime] = data;
 
-	layerControl.addOverlay(velocityLayer, 'Current');
-});
+        if (i === 13) {
+                // Sort the times array
+            times.sort(function(a, b) {
+            return new Date(a) - new Date(b);
+            });
+            // Initialize time slider
+            initTimeSlider();
+            // Initialize the persistent velocity layer
+            initVelocityLayer();
+        }
+    });
+}
+
+function initTimeSlider() {
+    var timeSlider = $('#time-slider');
+    var playButton = $('#play');
+    var stopButton = $('#stop');
+    var forwardButton = $('#forward');
+    var backwardButton = $('#backward');
+    var currentTime = $('#current-time');
+
+    timeSlider.attr('max', times.length - 1);
+
+    timeSlider.on('input', function () {
+        var timeIndex = this.value;
+        updateLayer(times[timeIndex]);
+        currentTime.text(times[timeIndex]);
+    });
+
+    playButton.on('click', function () {
+        playSlider();
+    });
+
+    stopButton.on('click', function () {
+        stopSlider();
+    });
+
+    forwardButton.on('click', function () {
+        moveSlider(1);
+    });
+
+    backwardButton.on('click', function () {
+        moveSlider(-1);
+    });
+
+    // Initialize with the first time
+    currentTime.text(times[0]);
+}
+
+function moveSlider(step) {
+    var timeSlider = $('#time-slider');
+    var newVal = +timeSlider.val() + step;
+    if (newVal >= 0 && newVal < times.length) {
+        timeSlider.val(newVal).trigger('input');
+    }
+}
+
+function initVelocityLayer() {
+    velocityLayer = L.velocityLayer({
+        displayValues: true,
+        displayOptions: {
+            velocityType: 'Wind',
+            displayPosition: 'bottomleft',
+            displayEmptyString: 'No wind data'
+        },
+        data: dataCache[times[0]],
+        maxVelocity: 10
+    });
+    layerControl.addOverlay(velocityLayer, 'Current');
+    velocityLayer.addTo(map);
+}
+
+function updateLayer(time) {
+    velocityLayer.setData(dataCache[time]);
+}
+
+var playInterval;
+function playSlider() {
+    var timeSlider = $('#time-slider');
+    playInterval = setInterval(function () {
+        if (timeSlider.val() < times.length - 1) {
+            timeSlider.val(+timeSlider.val() + 1).trigger('input');
+        } else {
+            clearInterval(playInterval);
+        }
+    }, 1000);
+}
+
+function stopSlider() {
+    clearInterval(playInterval);
+}
